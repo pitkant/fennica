e47a35e2a3c66b435b84c4853d2565c05c0a1de4
Niko Ilomäki <niko.ilomaki@helsinki.fi>
2015-02-19 00:27:26 +0200

    pics

--- inst/examples/main.R
+++ inst/examples/main.R
@@ -4,7 +4,7 @@
 
 # Define here the input file and output folder
 # The rest should then execute out-of-the box
-fennica.data.file <- "data/fennica.csv.gz"
+fennica.data.file <- "inst/examples/data/fennica.csv.gz"
 output.folder <- "output.tables/"
 
 # Create the output directory if not yet exists
@@ -17,11 +17,12 @@ library(dplyr)
 # Load R package
 # library(fennica)
 
+source("R/data.R")
 print("Read raw data")
 df.orig <- read_fennica_data(fennica.data.file)
 
 # Preprocess the raw data
-source("preprocessing.R")
+source("inst/examples/preprocessing.R")
 
 # Analyze the preprocessed data
 # source("analysis.R")
--- inst/examples/preprocessing.R
+++ inst/examples/preprocessing.R
@@ -386,27 +386,43 @@ df$pub_when <- fix_pubwhen(df$pub_when)
 df$pub_from <- fix_pubfrom(df$pub_from)
 df$pub_till <- fix_pubtill(df$pub_till)
 
-counts_by_town <- function(ldf, str) {
-  f <- ldf %>% filter(pub_where == str) %>% group_by(pub_when) %>% tally() %>% arrange(pub_when)
+counts_total <- function(ldf) {
+  f <- ldf %>% group_by(pub_when) %>% tally() %>% arrange(pub_when)
   f$cumul <- cumsum(f$n)
-
-  png(paste0(str,"_noncumul.png"))
+  
+  png("inst/examples/figure/total_noncumul.png")
   plot(f$pub_when,f$n)
   dev.off()
   
-  png(paste0(str,"_cumul.png"))
+  png("inst/examples/figure/total_cumul.png")
   plot(f$pub_when,f$cumul)
   dev.off()
   
 }
 
-counts_by_town(df,"Helsinki")
-counts_by_town(df,"Turku")
-counts_by_town(df,"Porvoo")
-counts_by_town(df,"Tukholma")
-counts_by_town(df,"Vaasa")
-counts_by_town(df,"Tampere")
-counts_by_town(df,"Kuopio")
-counts_by_town(df,"Hämeenlinna")
-counts_by_town(df,"Viipuri")
-counts_by_town(df,"Jyväskylä")
+counts_total(df)
+
+counts_by_town <- function(ldf, str, file) {
+  f <- ldf %>% filter(pub_where == str) %>% group_by(pub_when) %>% tally() %>% arrange(pub_when)
+  f$cumul <- cumsum(f$n)
+
+  png(paste0("inst/examples/figure/",file,"_noncumul.png"))
+  plot(f$pub_when,f$n)
+  dev.off()
+  
+  png(paste0("inst/examples/figure/",file,"_cumul.png"))
+  plot(f$pub_when,f$cumul)
+  dev.off()
+  
+}
+
+counts_by_town(df,"Helsinki", "Helsinki")
+counts_by_town(df,"Turku", "Turku")
+counts_by_town(df,"Porvoo", "Porvoo")
+counts_by_town(df,"Tukholma", "Tukholma")
+counts_by_town(df,"Vaasa", "Vaasa")
+counts_by_town(df,"Tampere", "Tampere")
+counts_by_town(df,"Kuopio","Kuopio")
+counts_by_town(df,"Hämeenlinna","Hameenlinna")
+counts_by_town(df,"Viipuri","Viipuri")
+counts_by_town(df,"Jyväskylä","Jyvaskyla")
