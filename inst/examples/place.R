9f3977f1561e8fa958f85737209e384103bff77e
Niko Ilomäki <niko.ilomaki@helsinki.fi>
2015-02-19 15:32:52 +0200

    extended place name coverage

--- inst/examples/place.R
+++ inst/examples/place.R
@@ -5,67 +5,136 @@ fix_placenames <- function (v) {
   v <- gsub("–"," ",v)
   v <- gsub("\\-"," ",v)
   
-  v <- gsub("Tryckt i ","",v)
-  v <- gsub("Tryckt j ","",v)  
-  v <- gsub(" prändäty","",v)
-  v <- gsub(" prändätty","",v)
-  v <- gsub(" pränt","",v)
-  v <- gsub(" präntäty","",v)
-  v <- gsub(" präntätty","",v)
-  v <- gsub("Prändäty ","",v)
-  v <- gsub("Prändätty ","",v)
-  v <- gsub("Pränt ","",v)
-  v <- gsub("Präntäty ","",v)
-  v <- gsub("Präntätty ","",v)
+  v <- gsub("^Tryckt i ","",v)
+  v <- gsub("^Tryckt j ","",v)
+  v <- gsub("^Prändäty ","",v)
+  v <- gsub("^Prändätty ","",v)
+  v <- gsub("^Pränt ","",v)
+  v <- gsub("^Präntäty ","",v)
+  v <- gsub("^Präntätty ","",v)
   v <- gsub("^A ","",v)
   v <- gsub("^Excusa ","",v)
   v <- gsub("^I ","",v)
-  v <- gsub(" caupungis$","",v)
+  v <- gsub("^Impr. ","",v)
+  v <- gsub("^Impressa ","",v)
+  v <- gsub("^Excusum ","",v)
+  v <- gsub("^Excudit ","",v)
+  v <- gsub("^Exc. ","",v)
+  v <- gsub("^En ","",v)
   
-  helsinki <- c("Helsingfors","Helsingissä","Helsingforsiae","Gel'singfors","Hki")
+  v <- gsub(" prändäty$","",v)
+  v <- gsub(" prändätty$","",v)
+  v <- gsub(" pränt$","",v)
+  v <- gsub(" präntäty$","",v)
+  v <- gsub(" präntätty$","",v)
+  v <- gsub(" caupungis$","",v)
+  v <- gsub(" impressum$","",v)
+  v <- gsub(" impressit$","",v)
+  v <- gsub(" impre$","",v)
+  v <- gsub(" v. editum$","",v)
+  
+  altdorf <- c("Altdorfi")
+  v[v %in% altdorf] <- "Altdorf"
+  
+  giessen <- c("Giessae","Gissae","Giessae Cattorum")
+  v[v %in% giessen] <- "Giessen"
+  
+  goteborg <- c("Gothoburgi")
+  v[v %in% goteborg] <- "Göteborg"
+  
+  gottingen <- c("Goettingae","Gottingae")
+  v[v %in% gottingen] <- "Göttingen"
+  
+  greifswald <- c("Gryphiswaldiae","Gryphiae")
+  v[v %in% greifswald] <- "Greifswald"
+  
+  helsinki <- c("Helsingfors","Helsingissä","Helsingforsiae","Gel'singfors","Hki","Helsingissä] :;Helsinki")
   v[v %in% helsinki] <- "Helsinki"
   
-  turku <- c("Aboae","Åbo","Turusa","Abo","Turussa","Abogiae","Turuusa")
-  v[v %in% turku] <- "Turku"
+  jena <- c("Ienae","Jenae")
+  v[v %in% jena] <- "Jena"
   
-  tukholma <- c("Stockholm","Stockholmisa","Stockholmis","Stockholmiae","Stockholmin","Stockholman","Stockholme","Holmiae","Stokholmis")
-  v[v %in% tukholma] <- "Tukholma"
+  kaliningrad <- c("Regiomonti Borussorum")
+  v[v %in% kaliningrad] <- "Kaliningrad"
   
-  vaasa <- c("Wasa","Waasa","Wasasa","Vasa","Vasasa","Nikolaistad","Nikolainkaupunki")
-  v[v %in% vaasa] <- "Vaasa"
-  
-  uppsala <- c("Upsaliae","Upsala","Ubsaliae","Vpsala")
-  v[v %in% uppsala] <- "Uppsala"
-  
-  pietari <- c("S. Peterburg","Sanktpeterburg","St. Petersburg","Petrograd","St. Petersbourg")
-  v[v %in% pietari] <- "Pietari"
-  
-  tartto <- c("Dorpati Livonorum","Dorpati","Dorpat","Dorpt","Tartu")
-  v[v %in% tartto] <- "Tartto"
-  
-  tampere <- c("Tammerfors","Tampereella")
-  v[v %in% tampere] <- "Tampere"
-  
-  viipuri <- c("Wiipuri","Viborg","Wiborg","Vyborg","Viburgi","Wiburgi") # huom! Tanskassakin on Viborg
-  v[v %in% viipuri] <- "Viipuri"
-  
-  wittenberg <- c("Wittebergae","Witebergae")
-  v[v %in% wittenberg] <- "Wittenberg"
+  kiel <- c("Kiliae")
+  v[v %in% kiel] <- "Kiel"
   
   koopenhamina <- c("København","Kjøbenhavn","Köbenhavn","Kjöbenhavn")
   v[v %in% koopenhamina] <- "Kööpenhamina"
   
-  v[v=="Kristiania"] <- "Oslo"
-  v[v=="Moskva"] <- "Moskova"
-  v[v=="Rostochii"] <- "Rostock"
-  v[v=="Turengi"] <- "Turenki"
+  leiden <- c("Lugduni Batavorum","Ludguni Batavorum")
+  v[v %in% leiden] <- "Leiden"
+  
+  linkoping <- c("Lincopiae")
+  v[v %in% linkoping] <- "Linköping"
+  
+  lund <- c("Lundae","Londini Gothorum","Londini Goth","Lond. Scanorum","Londini Scanor","Londini Scanorum")
+  v[v %in% lund] <- "Lund"
+  
+  magdeburg <- c("Magdaeburgi")
+  v[v %in% magdeburg] <- "Magdeburg"
+  
+  marburg <- c("Marpurgi Cattorum","Marpvrgi Cattorvm")
+  v[v %in% marburg] <- "Marburg"
+  
+  moskova <- c("Moskva")
+  v[v %in% moskova] <- "Moskova"
+  
+  narva <- c("Narvae")
+  v[v %in% narva] <- "Narva"
+  
+  oslo <- c("Kristiania")
+  v[v %in% oslo] <- "Oslo"
+
+  pietari <- c("S. Peterburg","Sanktpeterburg","St. Petersburg","Petrograd","St. Petersbourg","S:t Petersburg","St Petersbourg")
+  v[v %in% pietari] <- "Pietari"
+  
+  parnu <- c("Pernaviae")
+  v[v %in% parnu] < "Pärnu"
+  
+  riika <- c("Riga")
+  v[v %in% riika] <- "Riika"
+  
+  rostock <- c("Rostochii","Rostochi","Rostochiae")
+  v[v %in% rostock] <- "Rostock"
+  
+  strangnas <- c("Stregnesij","Strengnesiae")
+  v[v %in% strangnas] <- "Strängnäs"
+
+  tampere <- c("Tammerfors","Tampereella")
+  v[v %in% tampere] <- "Tampere"
+  
+  tartto <- c("Dorpati Livonorum","Dorpati","Dorpat","Dorpt","Tartu","Dorpati Livonvm")
+  v[v %in% tartto] <- "Tartto"
+  
+  tukholma <- c("Stockholm","Stockholmisa","Stockholmis","Stockholmiae","Stockholmin","Stockholman","Stockholme","Holmiae","Stokholmis","Stokholmiae")
+  v[v %in% tukholma] <- "Tukholma"
+  
+  turenki <- c("Turengi")
+  v[v %in% turenki] <- "Turenki"
+  
+  turku <- c("Aboae","Åbo","Turusa","Abo","Turussa","Abogiae","Turuusa","Aboa","Aboae Finnorum","Åboae","Åbae")
+  v[v %in% turku] <- "Turku"
+  
+  uppsala <- c("Upsaliae","Upsala","Ubsaliae","Vpsala", "Vpsaliae","Upsalis","Vbsaliae","Upsalie","Upsalae")
+  v[v %in% uppsala] <- "Uppsala"
+  
+  vaasa <- c("Wasa","Waasa","Wasasa","Vasa","Vasasa","Nikolaistad","Nikolainkaupunki","Wasae")
+  v[v %in% vaasa] <- "Vaasa"
+  
+  viipuri <- c("Wiipuri","Viborg","Wiborg","Vyborg","Viburgi","Wiburgi") # huom! Tanskassakin on Viborg
+  v[v %in% viipuri] <- "Viipuri"
+  
+  wittenberg <- c("Wittebergae","Witebergae","Wittenbergae")
+  v[v %in% wittenberg] <- "Wittenberg"
+  
   v[v=="Kristiina"] <- "Kristiinankaupunki" # vai Ristiina?
   v <- gsub("^Reval.*$","Tallinna",v)
-  v[v=="Riga"] <- "Riika"
-  v[v=="Pernaviae"] <- "Pärnu"
   
   # useita kaupunkeja
   v[v=="Stockholmisa ja Turusa"] <- "Tukholma; Turku"
+  v[v=="Stockholm och Åbo"] <- "Tukholma; Turku"
   v[v=="Helsinki ;;Viipuri"] <- "Helsinki; Viipuri"
   
   # ruotsinkieliset
@@ -88,6 +157,7 @@ fix_placenames <- function (v) {
   v[v=="Karis"] <- "Karjaa"
   v[v=="Nystad"] <- "Uusikaupunki"
   v[v=="Kristinestad"] <- "Kristiinankaupunki"
+  v[v=="Willmanstrand"] <- "Lappeenranta"
   
   # englanninkieliset
   v[v=="London"] <- "Lontoo"
