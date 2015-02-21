a1b180bb0bedad6c730bc56347dc743b26dd44fb
Niko Ilomäki <niko.ilomaki@helsinki.fi>
2015-02-21 18:24:30 +0200

    environment for place names

--- inst/examples/place.R
+++ inst/examples/place.R
@@ -1,4 +1,3 @@
 source("inst/examples/preprocess_placenames.R", encoding = "UTF-8")
 source("inst/examples/collect_misspellings.R", encoding = "UTF-8")
 source("inst/examples/correct_misspellings.R", encoding = "UTF-8")
-source("inst/examples/delete_misspellings.R", encoding = "UTF-8")
