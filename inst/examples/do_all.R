d8069a8d1651429348bf82da6f50782a4c11191e
Niko Ilomäki <niko.ilomaki@helsinki.fi>
2015-05-18 16:16:57 +0300

    table of publishers

--- inst/examples/do_all.R
+++ inst/examples/do_all.R
@@ -1,3 +1,3 @@
-source("phase1.R",encoding="UTF-8")
-source("phase2.R",encoding="UTF-8")
-source("phase3.R",encoding="UTF-8")
+source("phase1.R",encoding="UTF-8") # fennica.csv.gz -> df.raw
+source("phase2.R",encoding="UTF-8") # df.raw -> df.orig
+source("phase3.R",encoding="UTF-8") # df.orig -> df
