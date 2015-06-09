acac0b5cee52bd0d0d5b337bb15b967019d2a2c7
antagomir <leo.lahti@iki.fi>
2015-06-09 14:11:17 +0300

    added cran build scripts

---
+++ inst/extras/build.cran.sh
@@ -0,0 +1,13 @@
+# For info on Travis R scripts, see
+# http://jtleek.com/protocols/travis_bioc_devel/
+
+# Roxygen tips:
+# http://r-pkgs.had.co.nz/man.html
+
+#/usr/local/bin/R CMD BATCH document.R
+/usr/local/bin/R CMD build ../../ --no-build-vignettes
+#/usr/local/bin/R CMD build ../../ 
+/usr/local/bin/R CMD check fennica_0.0.1.tar.gz
+#/usr/local/bin/R CMD check --as-cran fennica_0.1.2.tar.gz
+/usr/local/bin/R CMD INSTALL fennica_0.0.1.tar.gz
+#/usr/local/bin/R CMD BATCH document.R
