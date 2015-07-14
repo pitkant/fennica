4ef14c92472ca4a6a6ab6e76272f4c592ee6a660
antagomir <leo.lahti@iki.fi>
2015-07-14 13:15:47 +0300

    Rd files update

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -7,7 +7,7 @@
 #/usr/local/bin/R CMD BATCH document.R
 /usr/local/bin/R CMD build ../../ --no-build-vignettes
 #/usr/local/bin/R CMD build ../../ 
-/usr/local/bin/R CMD check fennica_0.0.1.tar.gz
-#/usr/local/bin/R CMD check --as-cran fennica_0.1.2.tar.gz
+#/usr/local/bin/R CMD check fennica_0.0.1.tar.gz
+/usr/local/bin/R CMD check --as-cran fennica_0.0.1.tar.gz
 /usr/local/bin/R CMD INSTALL fennica_0.0.1.tar.gz
 #/usr/local/bin/R CMD BATCH document.R
