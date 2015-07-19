41c3583fd60a745f6bfba65758f89c9af482f03b
Niko Ilomäki <niko.ilomaki@helsinki.fi>
2015-07-19 23:33:32 +0300

    fix

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,5 +1,7 @@
 #' @import dplyr
 
+globalVariables(c("published_in","publication_place","data_size"))
+
 .onAttach <- function(lib, pkg)
 {
 
@@ -8,7 +10,6 @@
   # Sys.setlocale(locale = "WINDOWS-1252")
 
   packageStartupMessage("fennica - Algorithms for Finnish Library Data.\nCopyright (C) 2014-2015 Niko Ilomaki, Leo Lahti, and Mikko Tolonen\n\nhttps://github.com/ropengov/fennica \n\n Some scientists will seek and develop new collaborative arrangements; have members from all fields of science; and these new ways of working, effectively instrumented by huge computers, will contribute greatly to the advance in handling the complex, but essentially organic, problems of the biological and social sciences. - Weaver 1948 \n")
-  globalVariables(c("published_in","publication_place","data_size"))
   data_size <- 0
 
 }
