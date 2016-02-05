b37f398753f5a1c32c59534277b50ba26d600a70
antagomir <leo.lahti@iki.fi>
2016-02-05 13:04:38 +0100

    ok

--- inst/examples/main.R
+++ inst/examples/main.R
@@ -8,6 +8,7 @@ dir.create(output.folder)
 source("init.R")
 source("import2.R")    # Data preprocessing
 source("enrich.R")
+source("validation.R")
 saveRDS(df.preprocessed, "df.Rds")
 source("summarize.R") # Summary tables
 source("knit_all.R")  # Summary md docs
