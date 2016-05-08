ddcab3bef1660e7ae484ed69fbf68f66c775c030
antagomir <leo.lahti@iki.fi>
2016-05-08 20:07:30 +0200

    ok

--- inst/examples/analysis.R
+++ inst/examples/analysis.R
@@ -29,11 +29,22 @@ knitr::knit("20160303-Krakow.Rmd", encoding="UTF-8")
 #render("suvut.Rmd", output_format = "pdf_document")
 #render("20151209-HelsinkiDH.Rmd", output_format = "pdf_document")
 
-system("git add -f figure/*.png")
-# system("git add -f output.tables/*.csv")
-system("git add output.tables/*.csv")
-system("git commit -a -m'Rmd update'")
+# ----------------------------------------
+
+# Push changes to github
+# https://github.com/ropensci/git2r
+library(git2r)
+repo <- repository("../../")
+#config(repo, user.name="antagomir", user.email="leo.lahti@iki.fi")
+add(repo, "figure/*.png")
+commit(repo, "Rmd update")
+# push(repo) # does not work ?
 system("git push")
+
+# system("git add -f figure/*.png")
+# system("git add output.tables/*.csv")
+#system("git commit -a -m'Rmd update'")
+#system("git push")
 
 
 
