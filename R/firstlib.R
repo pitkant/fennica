5fab4f314ab14996b9f5eda0ea64b3782b7e1590
antagomir <leo.lahti@iki.fi>
2016-06-20 20:50:10 +0200

    ok

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,4 +1,7 @@
 #' @import dplyr
+#' @importFrom utils adist
+#' @importFrom utils na.omit
+#' @importFrom utils read.csv
 
 globalVariables(c("published_in","publication_place"))
 
@@ -9,6 +12,6 @@ globalVariables(c("published_in","publication_place"))
   # Sys.setlocale(locale = "UTF-8")
   # Sys.setlocale(locale = "WINDOWS-1252")
 
-  packageStartupMessage("fennica - Algorithms for Finnish Library Data.\nCopyright (C) 2014-2016 Niko Ilomaki, Leo Lahti, and Mikko Tolonen\n\nhttps://github.com/ropengov/fennica \n\n Some scientists will seek and develop new collaborative arrangements; have members from all fields of science; and these new ways of working, effectively instrumented by huge computers, will contribute greatly to the advance in handling the complex, but essentially organic, problems of the biological and social sciences. - Weaver 1948 \n")
+  packageStartupMessage("fennica - Algorithms for Finnish Library Data.\nCopyright (C) 2014-2016 Leo Lahti, Niko Ilomaki, and Mikko Tolonen\n\nhttps://github.com/ropengov/fennica \n\n Some scientists will seek and develop new collaborative arrangements; have members from all fields of science; and these new ways of working, effectively instrumented by huge computers, will contribute greatly to the advance in handling the complex, but essentially organic, problems of the biological and social sciences. - Weaver 1948 \n")
 
 }
