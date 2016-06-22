be083a8ec04daf15c3f391e3733f78869af1ab06
Leo Lahti <leo.lahti@iki.fi>
2016-06-22 13:08:40 +0200

    ok

---
+++ inst/examples/krakow.R
@@ -0,0 +1,19 @@
+
+library(rmarkdown)
+rmarkdown::render("manuscript-krakow.Rmd")
+#knitr::knit("manuscript-krakow.Rmd", encoding="UTF-8")
+
+# Push changes to github
+# https://github.com/ropensci/git2r
+library(git2r)
+repo <- repository("../../")
+#config(repo, user.name="antagomir", user.email="leo.lahti@iki.fi")
+add(repo, "figure/*.png", force = TRUE)
+# commit(repo, "Rmd update")
+# push(repo) # does not work ?
+#system("git push")
+
+# system("git add -f figure/*.png")
+# system("git add output.tables/*.csv")
+system("git commit -a -m'Rmd update'")
+system("git push")
