49e35d0c485f87582a1d1e75804cf59465eacb8a
hegroiva <hegroiva@users.noreply.github.com>
2016-07-02 15:46:26 +0300

    Added analysis files for combined catalogs
    Added analysis files for combined catalogs

--- inst/examples/analysis.combined.R
+++ inst/examples/analysis.combined.R
@@ -5,26 +5,43 @@ library(dplyr)
 min.year = 1470
 max.year = 1828
 setwd("~/GitHub/fennica/inst/examples/")
-fen <- readRDS("df.Rds")
-kun <- readRDS("../../../kungliga/antagomir/df.Rds")
 
-fenraw <- readRDS("df.raw.Rds")
-kunraw <- readRDS("../../../kungliga/antagomir/df.raw.Rds")
+if (file.exists("df.combined.Rds")) {
+  message("Opening df.combined.Rds")
+  df.combined.preprocessed <- readRDS("df.combined.Rds")
+} else {
+  message("Creating df.combined.Rds by combining df.Rds-files from Kungliga & Fennica")
+  
+  message("Reading Fennica df.Rds")
+  fen <- readRDS("df.Rds")
+  message("Reading Kungliga df.Rds")
+  kun <- readRDS("../../../kungliga/antagomir/df.Rds")
+  
+  message("Reading Fennica df.raw.Rds")
+  fenraw <- readRDS("df.raw.Rds")
+  message("Reading Kungliga df.raw.Rds")
+  kunraw <- readRDS("../../../kungliga/antagomir/df.raw.Rds")
+  
+  message("Filtering by years")
+  fen <- fen %>% filter(publication_year >= min.year & publication_year <= max.year)
+  kun <- kun %>% filter(publication_year >= min.year & publication_year <= max.year)
+  
+  message("Binding & adding extra fields")
+  df0 = bind_rows(fen, kun)
+  df0$catalog = factor(c(rep("Fennica", nrow(fen)), rep("Kungliga", nrow(kun))))
+  df0$catalog_index <- 0
+  df0$catalog_index[1:nrow(fen)] = 1:nrow(fen)
+  df0$catalog_index[(nrow(fen)+1):nrow(df0)] <- 1:nrow(kun)
+  
+  df.combined.preprocessed <- df0
+  
+  # This actually only adds $remove and fills it with values (just in case)
+  df.combined.preprocessed <- remove_duplicates(df.combined.preprocessed)
+  saveRDS(object=df.combined.preprocessed, file="df.combined.Rds")
+}
 
-fen <- fen %>% filter(publication_year >= min.year & publication_year <= max.year)
-kun <- kun %>% filter(publication_year >= min.year & publication_year <= max.year)
-df0 = bind_rows(fen, kun)
-df0$catalog = factor(c(rep("Fennica", nrow(fen)), rep("Kungliga", nrow(kun))))
-df0$catalog_index <- 0
-df0$catalog_index[1:nrow(fen)] = 1:nrow(fen)
-df0$catalog_index[(nrow(fen)+1):nrow(df0)] <- 1:nrow(kun)
-
-df.preprocessed <- df0
-
-# This actually only adds $remove and fills it with values (just in case)
-df.preprocessed <- remove_duplicates(df.preprocessed)
-
+message("Filtering out rows with remove-tags")
 # This is the real removal
-df.preprocessed <- df.preprocessed %>% filter(remove != "remove")
+df.filtered.preprocessed <- df.combined.preprocessed %>% filter(remove != "remove")
 
-saveRDS(object=df.preprocessed, file="df.combined.RDS")
+
---
+++ inst/examples/analysis.combined.run.R
@@ -0,0 +1,54 @@
+# LIST DISSERTATIONS
+output.folder <- "output.tables/"
+
+# List all dissertations
+dissertations <- filter_dissertations(df.filtered.preprocessed)
+fields <- c("catalog", "author", "short_title", "title", "dissertation", "publication_place", "publication_year")
+dissertations <- dissertations[,fields][order(dissertations$publication_year, dissertations$short_title, dissertations$publication_place, dissertations$catalog),]
+write.table(dissertations, paste0(output.folder, "dissertations_all.csv"), quote = FALSE, sep = "\t", row.names = FALSE)
+
+# List dissertations in Fennica
+dissertations <- filter_dissertations(df.filtered.preprocessed, catalog_name="Fennica")
+fields <- c("catalog", "author", "short_title", "title", "dissertation", "publication_place", "publication_year")
+dissertations <- dissertations[,fields][order(dissertations$publication_year, dissertations$short_title, dissertations$publication_place, dissertations$catalog),]
+write.table(dissertations, paste0(output.folder, "dissertations_fennica.csv"), quote = FALSE, sep = "\t", row.names = FALSE)
+
+# List dissertations in Kungliga
+dissertations <- filter_dissertations(df.filtered.preprocessed, catalog_name="Kungliga")
+fields <- c("catalog", "author", "short_title", "title", "dissertation", "publication_place", "publication_year")
+dissertations <- dissertations[,fields][order(dissertations$publication_year, dissertations$short_title, dissertations$publication_place, dissertations$catalog),]
+write.table(dissertations, paste0(output.folder, "dissertations_kungliga.csv"), quote = FALSE, sep = "\t", row.names = FALSE)
+
+# List dissertations by publication place
+town <- "Turku"
+dissertations <- filter_dissertations(df.filtered.preprocessed, town=town)
+fields <- c("catalog", "author", "short_title", "title", "dissertation", "publication_place", "publication_year")
+dissertations <- dissertations[,fields][order(dissertations$publication_year, dissertations$short_title, dissertations$publication_place, dissertations$catalog),]
+write.table(dissertations, paste0(output.folder, "dissertations_", town, ".csv"), quote = FALSE, sep = "\t", row.names = FALSE)
+
+# List dissertation mismatches
+mismatches <- get_dissertation_mismatch(df.combined.preprocessed)
+fields <- c("catalog", "author", "short_title", "title", "dissertation", "publication_place", "publication_year")
+mismatches <- mismatches[,fields][order(mismatches$publication_year, mismatches$short_title, mismatches$publication_place, mismatches$catalog),]
+write.table(dissertations, paste0(output.folder, "dissertation_mismatches.csv"), quote = FALSE, sep = "\t", row.names = FALSE)
+
+# List anything in Kungliga, which is printed in Finland, town by town
+for (town in c("Turku", "Helsinki", "Vyborg", "Vaasa")) {
+  publications <- filter_towns(df.filtered.preprocessed, catalog_name="Kungliga", town=town)
+  fields <- c("catalog", "author", "short_title", "title", "dissertation", "publication_place", "publication_year")
+  publications <- publications[,fields][order(publications$publication_year, publications$short_title, publications$publication_place, publications$catalog),]
+  write.table(publications, paste0(output.folder, town, "_in_Kungliga_only.csv"), quote = FALSE, sep = "\t", row.names = FALSE)
+}
+
+# List duplicates
+duplicates <- filter_duplicates(df.combined.preprocessed)
+fields <- c("catalog", "author", "short_title", "title", "dissertation", "publication_place", "publication_year")
+duplicates <- duplicates[,fields][order(duplicates$publication_year, duplicates$short_title, duplicates$publication_place, duplicates$catalog),]
+write.table(duplicates, paste0(output.folder, "duplicates.csv"), quote = FALSE, sep = "\t", row.names = FALSE)
+
+#tmp <- generate_summary_tables(df.preprocessed, df.orig, output.folder)
+#sf <- generate_summaryfiles(df.preprocessed, author = author, output.folder = output.folder, ntop = ntop)
+
+
+
+
