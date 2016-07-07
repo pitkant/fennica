7b6f86b0acdd5f45db95bdcca0b8fb979c38941c
Mikko Tolonen <mikko.tolonen@helsinki.fi>
2016-07-07 18:14:27 +0300

    a

--- inst/examples/tmp.R
+++
@@ -1,8 +0,0 @@
-
-df <- filter(df0, catalog == "Kungliga") %>%
-      filter(publication_year <= 1828)
-
-output.folder <- "figure_201606_Krakow/"
-field <- "subject_topic"
-entries = unlist(strsplit(as.character(df.preprocessed[[field]]), ";"), use.names = FALSE)
-s <- write_xtable(entries, file = paste(output.folder, catalog, "_subjecttopics", min(df$publication_year), "_", max(df$publication_year), ".csv", sep = ""))
