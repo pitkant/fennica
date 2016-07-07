ac2437eb7eb70eb1ebfb263b75ac939b8711702f
antagomir <leo.lahti@iki.fi>
2016-07-07 02:02:07 +0200

    slide updates

--- inst/examples/tmp.R
+++ inst/examples/tmp.R
@@ -1,8 +1,58 @@
+library(dplyr)
+library(ggmap)
+library(ggplot2)
+library(gisfin)
+source("funcs.R")
 
-df <- filter(df0, catalog == "Kungliga") %>%
-      filter(publication_year <= 1828)
+theme_set(get_theme_map())
+mapdata <- filter(df0, catalog == "Fennica") %>%
+      group_by(publication_decade, publication_place, latitude, longitude) %>%
+      summarise(n = n())
 
-output.folder <- "figure_201606_Krakow/"
-field <- "subject_topic"
-entries = unlist(strsplit(as.character(df.preprocessed[[field]]), ";"), use.names = FALSE)
-s <- write_xtable(entries, file = paste(output.folder, catalog, "_subjecttopics", min(df$publication_year), "_", max(df$publication_year), ".csv", sep = ""))
+mapdata$size <- mapdata$n
+mapdata$highlight <- rep("black", nrow(mapdata))
+mapdata$highlight[mapdata$publication_place %in% c("Turku")] <- "red"
+mapdata$highlight <- factor(mapdata$highlight)
+
+#---------------------------------------
+
+region <- "Europe.north"
+mymap <- get_map(location=geobox(region), color = "bw", source="google",  maptype="terrain")
+
+
+print("Initialize plot")
+p0 <- ggmap(mymap) + theme(legend.position="none")
+
+pics <- list()
+for (i in c(1650, 1710, 1760, 1800)) {
+  
+    # Plot map
+    p <- p0
+
+    # Pick the investigated period (sliding window)
+    # Sum up the years within this sliding window for each element    	    
+    dfw <- subset(mapdata, publication_decade == i) %>%
+    	   	       group_by(latitude, longitude, highlight) %>%
+		       summarize(n = sum(size))
+
+    if (nrow(dfw) > 0) {
+      p <- p0 +
+      	geom_point(data = dfw,
+      	  aes(x = longitude, y = latitude,
+	      size = 20*log10(1+n),
+	      color = highlight)) +
+	      #color = highlight), alpha = 0.8) +	      
+	  ggtitle(i) +
+	  scale_size(range = c(2, 20)) +
+	  scale_color_manual(values = c("black", "red"))	  
+    }
+    
+  pics[[as.character(i)]] <- p
+
+}
+
+library(gridExtra)
+grid.arrange(pics[[1]], pics[[2]], pics[[3]], pics[[4]], nrow = 1)
+
+
+
