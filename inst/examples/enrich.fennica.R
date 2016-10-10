06e1cf709db3290a6eef44e552340f84915dd22d
Ville Vaara <villepvaara@gmail.com>
2016-10-10 17:53:13 +0300

    mostly done. needs more testing

--- inst/examples/enrich.fennica.R
+++ inst/examples/enrich.fennica.R
@@ -1,100 +1,110 @@
-message("Enriching Fennica")
+enrich_fennica <- function(data.enriched) {
 
-message("Identify issues")
-df.preprocessed$issue <- is.issue(df.preprocessed)
+  df.preprocessed <- data.enriched$df.preprocessed
+  update.fields <-   data.enriched$update.fields
+  conversions <-     data.enriched$conversions
 
-## COMBINE PUBLICATION-YEAR AND PUBLICATION-INTERVAL FIELDS
-# Recognize issues: those that have publication interval or frequency defined
-message("Combining year and interval")
-# Assume that all cases with denoted interval have start and end year
-# If one is missing, then it means that start and end are the same year
-# therefore fill those missing entries here:s
-df.preprocessed$publication_interval_from[is.na(df.preprocessed$publication_interval_from)] <- df.preprocessed$publication_interval_till[is.na(df.preprocessed$publication_interval_from)]
-df.preprocessed$publication_interval_till[is.na(df.preprocessed$publication_interval_till)] <- df.preprocessed$publication_interval_from[is.na(df.preprocessed$publication_interval_till)]
+  message("Enriching Fennica")
 
-# Discard erroneous years
-df.preprocessed$publication_interval_from[df.preprocessed$publication_interval_from < 1400] <- NA
-df.preprocessed$publication_interval_till[df.preprocessed$publication_interval_till < 1400] <- NA
-df.preprocessed$publication_interval_from[df.preprocessed$publication_interval_from > 2000] <- NA
-df.preprocessed$publication_interval_till[df.preprocessed$publication_interval_till > 2000] <- NA
+  message("Identify issues")
+  df.preprocessed$issue <- is.issue(df.preprocessed)
 
+  ## COMBINE PUBLICATION-YEAR AND PUBLICATION-INTERVAL FIELDS
+  # Recognize issues: those that have publication interval or frequency defined
+  message("Combining year and interval")
+  # Assume that all cases with denoted interval have start and end year
+  # If one is missing, then it means that start and end are the same year
+  # therefore fill those missing entries here:s
+  df.preprocessed$publication_interval_from[is.na(df.preprocessed$publication_interval_from)] <- df.preprocessed$publication_interval_till[is.na(df.preprocessed$publication_interval_from)]
+  df.preprocessed$publication_interval_till[is.na(df.preprocessed$publication_interval_till)] <- df.preprocessed$publication_interval_from[is.na(df.preprocessed$publication_interval_till)]
 
-# Compare with publication year field.
-# When the non-NA entries are unique, use the same year for all
-tmp <- cbind(from0 = df.preprocessed$publication_year_from[inds],
-             till0 = df.preprocessed$publication_year_till[inds],
-	     from = df.preprocessed$publication_interval_from[inds],
-	     till = df.preprocessed$publication_interval_till[inds]
-)
-inds2 <- unname(which(apply(tmp, 1, function (x) {length(unique(na.omit(x)))}) == 1))
-y <- unname(apply(tmp[inds2,], 1, function (x) {unique(na.omit(x))}))
-df.preprocessed$publication_year_from[inds[inds2]] <- y
-df.preprocessed$publication_year_till[inds[inds2]] <- y
-# For conflicting years, select he largest combined span
-tmp <- cbind(from0 = df.preprocessed$publication_year_from[inds],
-             till0 = df.preprocessed$publication_year_till[inds],
-	     from = df.preprocessed$publication_interval_from[inds],
-	     till = df.preprocessed$publication_interval_till[inds]
-	     )
-mins <- unname(apply(tmp, 1, function (x) {min(x, na.rm = TRUE)}))
-maxs <- unname(apply(tmp, 1, function (x) {max(x, na.rm = TRUE)}))
-df.preprocessed$publication_year_from[inds] <- mins
-df.preprocessed$publication_year_till[inds] <- maxs
+  # Discard erroneous years
+  df.preprocessed$publication_interval_from[df.preprocessed$publication_interval_from < 1400] <- NA
+  df.preprocessed$publication_interval_till[df.preprocessed$publication_interval_till < 1400] <- NA
+  df.preprocessed$publication_interval_from[df.preprocessed$publication_interval_from > 2000] <- NA
+  df.preprocessed$publication_interval_till[df.preprocessed$publication_interval_till > 2000] <- NA
 
-# ------------------------------------------------------
+  inds <- which(is.na(df.preprocessed$publication_year))
 
-message("Updating publication times")
+  # Compare with publication year field.
+  # When the non-NA entries are unique, use the same year for all
+  tmp <- cbind(from0 = df.preprocessed$publication_year_from[inds],
+               till0 = df.preprocessed$publication_year_till[inds],
+               from = df.preprocessed$publication_interval_from[inds],
+               till = df.preprocessed$publication_interval_till[inds]
+               )
+  inds2 <- unname(which(apply(tmp, 1, function (x) {length(unique(na.omit(x)))}) == 1))
+  y <- unname(apply(tmp[inds2,], 1, function (x) {unique(na.omit(x))}))
+  df.preprocessed$publication_year_from[inds[inds2]] <- y
+  df.preprocessed$publication_year_till[inds[inds2]] <- y
+  # For conflicting years, select he largest combined span
+  tmp <- cbind(from0 = df.preprocessed$publication_year_from[inds],
+               till0 = df.preprocessed$publication_year_till[inds],
+               from = df.preprocessed$publication_interval_from[inds],
+               till = df.preprocessed$publication_interval_till[inds]
+               )
+  mins <- unname(apply(tmp, 1, function (x) {min(x, na.rm = TRUE)}))
+  maxs <- unname(apply(tmp, 1, function (x) {max(x, na.rm = TRUE)}))
+  df.preprocessed$publication_year_from[inds] <- mins
+  df.preprocessed$publication_year_till[inds] <- maxs
 
-# Use from field; if from year not available, then use till year
-df.preprocessed$publication_year <- df.preprocessed$publication_year_from
-inds <- which(is.na(df.preprocessed$publication_year))
-df.preprocessed$publication_year[inds] <- df.preprocessed$publication_year_till[inds]
+  # ------------------------------------------------------
 
-# publication_decade
-df.preprocessed$publication_decade <- floor(df.preprocessed$publication_year/10) * 10 # 1790: 1790-1799
+  message("Updating publication times")
 
-# ------------------------------------------------------
+  # Use from field; if from year not available, then use till year
+  df.preprocessed$publication_year <- df.preprocessed$publication_year_from
+  inds <- which(is.na(df.preprocessed$publication_year))
+  df.preprocessed$publication_year[inds] <- df.preprocessed$publication_year_till[inds]
 
-message("Custom gender information for Fennica")
-# For author names, use primarily the Finnish names database
-# hence use it to replace the genders assigned earlier by bibliographica
-library(fennica)
-firstname <- pick_firstname(df.preprocessed$author_name, format = "last, first")
+  # publication_decade
+  df.preprocessed$publication_decade <- floor(df.preprocessed$publication_year/10) * 10 # 1790: 1790-1799
 
-# Let us Finnish gender mappings override others
-gender.fi <- get_gender_fi()[, c("name", "gender")] # Finnish
-genderfi <- get_gender(firstname, gender.fi)
-inds <- which(!is.na(genderfi))
-df.preprocessed$author_gender[inds] <- genderfi[inds]
+  # ------------------------------------------------------
 
-# Let us Fennica custom gender mappings override others
-gender.custom <- read_mapping("custom_gender_fennica.csv", sep = "\t",
-	      	   from = "name", to = "gender", mode = "table")
-gendercustom <- get_gender(firstname, gender.custom)
-inds <- which(!is.na(gendercustom))
-df.preprocessed$author_gender[inds] <- gendercustom[inds]
+  message("Custom gender information for Fennica")
+  # For author names, use primarily the Finnish names database
+  # hence use it to replace the genders assigned earlier by bibliographica
+  library(fennica)
+  firstname <- pick_firstname(df.preprocessed$author_name, format = "last, first")
 
-# TODO set this in again when ready
-#message("-- Fennica publishers")
-#df.preprocessed.bu <- df.preprocessed
-df.preprocessed$publisher <- polish_publisher_fennica(df.preprocessed)
-#df.preprocessed$publisher <- rep(NA, nrow(df.preprocessed))
-      
-# ----------------------------------------------------------------
- 
-# Recognize synonymes with string matching;
-# this step can be skipped after the synonyme list is fixed
-# source("city_synonyme_list_update.R", encoding = "UTF-8") 
-# This was combined with ESTC generic list to simplify
-# TODO Think how to split and generalize
-# Finally manual harmonization for the remaining place names
-# Now all synonymes moved in ESTC
-#f <- system.file("extdata/publication_place_synonymes_fennica.csv", package = "fennica")
-#sn <- read.csv(f, sep = ";")
-#df.preprocessed$publication_place <- sorvi::harmonize_names(df.preprocessed$publication_place, synonymes = sn, check.synonymes = FALSE)$name
+  # Let us Finnish gender mappings override others
+  gender.fi <- get_gender_fi()[, c("name", "gender")] # Finnish
+  genderfi <- get_gender(firstname, gender.fi)
+  inds <- which(!is.na(genderfi))
+  df.preprocessed$author_gender[inds] <- genderfi[inds]
 
-# ----------------------------------------------------------------------
+  # Let us Fennica custom gender mappings override others
+  gender.custom <- read_mapping("custom_gender_fennica.csv", sep = "\t",
+               from = "name", to = "gender", mode = "table")
+  gendercustom <- get_gender(firstname, gender.custom)
+  inds <- which(!is.na(gendercustom))
+  df.preprocessed$author_gender[inds] <- gendercustom[inds]
 
+  # TODO set this in again when ready
+  #message("-- Fennica publishers")
+  #df.preprocessed.bu <- df.preprocessed
+  df.preprocessed$publisher <- polish_publisher_fennica(df.preprocessed)
+  #df.preprocessed$publisher <- rep(NA, nrow(df.preprocessed))
+        
+  # ----------------------------------------------------------------
+   
+  # Recognize synonymes with string matching;
+  # this step can be skipped after the synonyme list is fixed
+  # source("city_synonyme_list_update.R", encoding = "UTF-8") 
+  # This was combined with ESTC generic list to simplify
+  # TODO Think how to split and generalize
+  # Finally manual harmonization for the remaining place names
+  # Now all synonymes moved in ESTC
+  #f <- system.file("extdata/publication_place_synonymes_fennica.csv", package = "fennica")
+  #sn <- read.csv(f, sep = ";")
+  #df.preprocessed$publication_place <- sorvi::harmonize_names(df.preprocessed$publication_place, synonymes = sn, check.synonymes = FALSE)$name
 
+  # ----------------------------------------------------------------------
 
+  data.enriched <- list(df.preprocessed = df.preprocessed,
+                        update.fields = update.fields,
+                        conversions = conversions)
 
+  return (data.enriched)
+}
--- inst/examples/validation.fennica.R
+++ inst/examples/validation.fennica.R
@@ -1,25 +1,40 @@
-# Manually checked for Fennica - 3 publications before 1400;
-# in all cases it seems that this is misspelling and the original year cant be inferred from the entry
-max.year <- as.numeric(format(Sys.time(), "%Y")) # this.year
-min.year <- 1400
-df.preprocessed$publication_year_from[which(df.preprocessed$publication_year_from > max.year)] <- NA
-df.preprocessed$publication_year_from[which(df.preprocessed$publication_year_from < min.year)] <- NA
-df.preprocessed$publication_year_till[which(df.preprocessed$publication_year_till > max.year)] <- NA
-df.preprocessed$publication_year_till[which(df.preprocessed$publication_year_till < min.year)] <- NA
 
-# Subsequent correction to the publication year fields
-print("Publication times")
-# Use from field; if from year not available, then use till year
-df.preprocessed$publication_year <- df.preprocessed$publication_year_from
-inds <- which(is.na(df.preprocessed$publication_year))
-df.preprocessed$publication_year[inds] <- df.preprocessed$publication_year_till[inds]
-# publication_decade
-df.preprocessed$publication_decade <- floor(df.preprocessed$publication_year/10) * 10 # 1790: 1790-1799
+validation_fennica <- function(data.enriched.fennica) {
+
+  df.preprocessed <- data.enriched.fennica$df.preprocessed
+  update.fields <-   data.enriched.fennica$update.fields
+  conversions <-     data.enriched.fennica$conversions
+
+  # Manually checked for Fennica - 3 publications before 1400;
+  # in all cases it seems that this is misspelling and the original year cant be inferred from the entry
+  max.year <- as.numeric(format(Sys.time(), "%Y")) # this.year
+  min.year <- 1400
+  df.preprocessed$publication_year_from[which(df.preprocessed$publication_year_from > max.year)] <- NA
+  df.preprocessed$publication_year_from[which(df.preprocessed$publication_year_from < min.year)] <- NA
+  df.preprocessed$publication_year_till[which(df.preprocessed$publication_year_till > max.year)] <- NA
+  df.preprocessed$publication_year_till[which(df.preprocessed$publication_year_till < min.year)] <- NA
+
+  # Subsequent correction to the publication year fields
+  print("Publication times")
+  # Use from field; if from year not available, then use till year
+  df.preprocessed$publication_year <- df.preprocessed$publication_year_from
+  inds <- which(is.na(df.preprocessed$publication_year))
+  df.preprocessed$publication_year[inds] <- df.preprocessed$publication_year_till[inds]
+  # publication_decade
+  df.preprocessed$publication_decade <- floor(df.preprocessed$publication_year/10) * 10 # 1790: 1790-1799
 
 
 
-# Publication interval must be within 1400-2000
-df.preprocessed$publication_interval_from[df.preprocessed$publication_interval_from < 1400] <- NA
-df.preprocessed$publication_interval_from[df.preprocessed$publication_interval_from > 2000] <- NA
-df.preprocessed$publication_interval_till[df.preprocessed$publication_interval_till < 1400] <- NA
-df.preprocessed$publication_interval_till[df.preprocessed$publication_interval_till > 2000] <- NA
+  # Publication interval must be within 1400-2000
+  df.preprocessed$publication_interval_from[df.preprocessed$publication_interval_from < 1400] <- NA
+  df.preprocessed$publication_interval_from[df.preprocessed$publication_interval_from > 2000] <- NA
+  df.preprocessed$publication_interval_till[df.preprocessed$publication_interval_till < 1400] <- NA
+  df.preprocessed$publication_interval_till[df.preprocessed$publication_interval_till > 2000] <- NA
+
+  data.validated.fennica <- list(df.preprocessed = df.preprocessed,
+                            update.fields = update.fields,
+                            conversions = conversions)
+
+  return (data.validated.fennica)
+
+}
