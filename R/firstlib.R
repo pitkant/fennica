71afbba0369716ff6b238ebb3c9e022f1be080bc
Leo <leo.lahti@iki.fi>
2018-12-26 11:29:49 +0200

    sync

--- R/firstlib.R
+++ R/firstlib.R
@@ -27,6 +27,6 @@ globalVariables(c("published_in","publication_place"))
   # Sys.setlocale(locale = "UTF-8")
   # Sys.setlocale(locale = "WINDOWS-1252")
 
-  packageStartupMessage("fennica - Algorithms for Finnish Library Data.\nCopyright (C) 2014-2017 Leo Lahti, Hege Roivainen, and Mikko Tolonen\n\nhttps://github.com/COMHIS/fennica \n\n Some scientists will seek and develop new collaborative arrangements; have members from all fields of science; and these new ways of working, effectively instrumented by huge computers, will contribute greatly to the advance in handling the complex, but essentially organic, problems of the biological and social sciences. - Weaver 1948 \n")
+  packageStartupMessage("fennica - Algorithms for Finnish Library Data.\nCopyright (C) 2014-2019 Leo Lahti, Hege Roivainen, and Mikko Tolonen\n\nhttps://github.com/COMHIS/fennica \n\n Some scientists will seek and develop new collaborative arrangements; have members from all fields of science; and these new ways of working, effectively instrumented by huge computers, will contribute greatly to the advance in handling the complex, but essentially organic, problems of the biological and social sciences. - Weaver 1948 \n")
 
 }
