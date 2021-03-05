82c3ee92a4baa619ee5cbd33712ae2377e6f14dc
Julia <yulmat@utu.fi>
2021-03-05 03:50:16 -0800

    update

--- inst/examples/author_date.R
+++ inst/examples/author_date.R
@@ -3,8 +3,8 @@
 field <- "author_date"
 # TODO make a tidy cleanup function to shorten the code here
 df.tmp <- polish_years(df.orig[[field]], check = TRUE, verbose = FALSE)
-
-
+df.tmp <- df.tmp %>% dplyr::rename(author_birth = from) %>%
+  dplyr::rename(author_death = till)
 
 # Store the title field data
 # FIXME: convert to feather or plain CSV
